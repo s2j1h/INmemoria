16cb49e9ef947fc2ca24d07ddb566489223ed09e
Julien Raigneau <julien@zeneffy.fr>
2011-04-16 23:59:02 +0200

    basic authentification

--- app.rb
+++ app.rb
@@ -1,18 +1,53 @@
 require 'rubygems'
 require 'sinatra'
 require 'rack-flash'
+require 'haml'
 
 use Rack::Flash
 enable :sessions
 
- 
+set :token,'maketh1$longandh@rdtoremembeavecdesmotsenfrancaisr'
+
+configure :development do
+  set :username,'toto'
+  set :password,'tata'
+end
+
 configure :production do
   require 'newrelic_rpm'
 end
- 
-# Quick test
+
+helpers do
+  def admin? ; request.cookies[settings.username] == settings.token ; end
+  def protected! ;redirect 'login' unless admin? ; end
+end
+
 get '/' do
-  flash[:notice] = "Your note has been saved"
-  "Hello from the ratpack!"
+  haml :index
+end
+
+# Quick test
+get '/admin' do
+  protected!
+  haml :admin
+end
+
+get '/login' do
+  haml :login
+end
+
+post '/login' do
+  if params['username']==settings.username && params['password']==settings.password
+    response.set_cookie(settings.username,:value => settings.token,:expires => Time.new + 3600) 
+    redirect '/admin'
+  else
+    flash['error'] = "Username or Password incorrect"
+     redirect '/login'
+  end
+end
+
+get '/logout' do
+  response.set_cookie(settings.username, false)
+  redirect '/login'
 end
 
