6a5557e61898ec114eb9d8a35c260685702ef126
Julien Raigneau <julien@zeneffy.fr>
2011-04-22 17:25:42 +0200

    added picasa support

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :gemcutter
 gem 'sinatra', '1.2.2'
 gem 'newrelic_rpm', '2.14.1'
 gem 'koala' , '>1.0.0.beta'
-gem 'haml'
-gem 'rack-flash'
+gem 'haml', '3.0.25'
+gem 'rack-flash', '0.1.1'
+gem 'gdata','1.1.2'
 
--- app.rb
+++ app.rb
@@ -2,12 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'rack-flash'
 require 'haml'
+require 'gdata'
 
 use Rack::Flash
 enable :sessions
 
 set :password, ENV['password'] || 'secret'
 set :username, ENV['username'] || 'secret'
+set :picasa_password, ENV['picasa_password'] 
+set :picasa_username, ENV['picasa_username']
 set :token,'maketh1$longandh@rdtoremembeavecdesmotsenfrancaisr'
 
 configure :production do
@@ -19,32 +22,77 @@ helpers do
   def protected! ;redirect 'login' unless admin? ; end
 end
 
-get '/' do
-  haml :index
+# Creates a GData client for picasa.
+def picasa_client
+  client = GData::Client::Photos.new
+  client.clientlogin(settings.picasa_username,settings.picasa_password)
+  client
 end
 
-# Quick test
+
+get '/' do
+  haml :index, :layout => false
+end
+
+# Admin pages with list 
 get '/admin' do
   protected!
   haml :admin
 end
 
+#GET login page
 get '/login' do
   haml :login
 end
 
+#Check login and rediret to admin page
 post '/login' do
   if params['username']==settings.username && params['password']==settings.password
     response.set_cookie(settings.username,:value => settings.token,:expires => Time.new + 3600) 
     redirect '/admin'
   else
-    flash['error'] = "Username or Password incorrect"
-     redirect '/login'
+    flash['error'] = "Login ou mot de passe incorrects"
+    redirect '/login'
   end
 end
 
 get '/logout' do
   response.set_cookie(settings.username, false)
+  flash['info'] = "Vous êtes déconnecté"
   redirect '/login'
+end
+
+get '/add' do
+  protected!
+  haml :add
+end
+
+post '/add' do
+  protected!
+  unless params[:image] && (tmpfile = params[:image][:tempfile]) && (name = params[:image][:filename])
+    flash['error'] = "Merci de remplir l'ensemble des informations obligatoires" 
+    redirect '/add'
+  end
+  file = Tempfile.new(['hello', '.jpg'])
+  while blk = tmpfile.read(65536)
+    file.write(blk)
+  end
+  client = picasa_client
+  test_image = file.path
+  mime_type = 'image/jpeg'
+    
+  response = client.post_file('http://picasaweb.google.com/data/feed/api/user/default/albumid/default', test_image, mime_type).to_xml
+    
+  puts response.elements["media:group"].elements["media:content"].attributes['url']
+  puts response.elements["media:group"].elements["media:thumbnail[@width='288']"].attributes['url']
+  tmpfile.close
+  tmpfile.unlink
+  edit_uri = response.elements["link[@rel='edit']"].attributes['href']
+  #client.delete(edit_uri)
+
+
+  flash['info'] = "nouvelle entrée créée"
+  redirect '/admin'
+  
 end
 
