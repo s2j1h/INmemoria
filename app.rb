948a69ee8cdeec282b0d31ec1e01a615f6d5d29d
Julien Raigneau <julien@zeneffy.fr>
2011-04-25 16:37:54 +0200

    corrected a serious bug with uploads (missing bits)

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'haml'
 require 'gdata'
 require 'sinatra/redirect_with_flash'
 require 'data_mapper'
-require "ftools"
 
 use Rack::Flash, :sweep => true
 enable :sessions
@@ -105,15 +104,6 @@ post '/admin/add' do
   tmpfile = params[:image][:tempfile]
   filename = params[:image][:filename]
 
-  file = Tempfile.new([filename, '.jpg'])
-  #tmpfile.rewind
-  #file.write(tmpfile.read)
-
-  File.copy(tmpfile.path,file.path)
-
-  #while blk = tmpfile.read(65536)
-  #  file.write(blk)
-  #end
   client = picasa_client
   test_image = tmpfile.path
   mime_type = 'image/jpeg'
@@ -190,11 +180,7 @@ post '/admin/edit/:id' do
   tmpfile = params[:image][:tempfile]
   filename = params[:image][:filename]
 
-  file = Tempfile.new([filename, '.jpg'])
-  while blk = tmpfile.read(65536)
-    file.write(blk)
-  end
-  test_image = file.path
+  test_image = tmpfile.path
   mime_type = 'image/jpeg'
     
   response = client.post_file('http://picasaweb.google.com/data/feed/api/user/default/albumid/default', test_image, mime_type).to_xml
