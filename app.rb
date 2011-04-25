fbf4270863920d7909c7d83169a667651a583be9
Julien Raigneau <julien@zeneffy.fr>
2011-04-25 15:28:24 +0200

    admin almost done

--- app.rb
+++ app.rb
@@ -28,18 +28,18 @@ end
 class Hommage
   include DataMapper::Resource  
   property :id,                   Serial
-  property :nom,                  String, :required => true
+  property :nom,                  String
   property :dateNaissance,        String
-  property :dateDeces,            String, :required => true
+  property :dateDeces,            String
   property :commentaires,         Text
-  property :urlImagePleine,       Text, :required => true
-  property :urlImageReduite,      Text, :required => true
-  property :urlImageTresReduite,  Text, :required => true
-  property :urlImageEdit,         Text, :required => true
+  property :urlImagePleine,       Text
+  property :urlImageReduite,      Text
+  property :urlImageTresReduite,  Text
+  property :urlImageEdit,         Text
 end
 
 DataMapper.auto_upgrade!
-#DataMapper::Model.raise_on_save_failure = true #permet de savoir si tout est bien sauvegardé, à utiliser avec rescue
+DataMapper::Model.raise_on_save_failure = true #permet de savoir si tout est bien sauvegardé, à utiliser avec rescue
 
 helpers do
   def admin? ; request.cookies[settings.username] == settings.token ; end
@@ -86,19 +86,19 @@ get '/logout' do
   haml :login
 end
 
-get '/add' do
+get '/admin/add' do
   protected!
   haml :add
 end
 
-post '/add' do
+post '/admin/add' do
   protected!
   if params[:nom] == "" || params[:dateDeces] == ""
-    redirect '/add', :error =>  "Merci de remplir l'ensemble des informations obligatoires" 
+    redirect '/admin/add', :error =>  "Merci de remplir l'ensemble des informations obligatoires" 
   end
 
   unless params[:image] && params[:image][:tempfile] && params[:image][:filename]
-    redirect '/add', :error =>  "Merci de remplir l'ensemble des informations obligatoires" 
+    redirect '/admin/add', :error =>  "Merci de remplir l'ensemble des informations obligatoires" 
   end
   tmpfile = params[:image][:tempfile]
   filename = params[:image][:filename]
@@ -140,10 +140,103 @@ post '/add' do
      redirect '/admin', :notice => "Une nouvelle entrée a été créée"
   else
     puts hommage.errors.inspect
-    redirect '/admin', :error => "Une erreur a empéché la sauvegarde de l'hommage - merci de contacter votre admin préféré"
+    redirect '/admin', :error => "Une erreur a empêché la sauvegarde de l'hommage - merci de contacter votre admin préféré"
   end
-   
 end
+
+get '/admin/edit/:id' do
+  protected!
+  @hommage = Hommage.get(params[:id])
+  haml :edit
+end
+
+post '/admin/edit/:id' do
+  protected!
+  if params[:nom] == "" || params[:dateDeces] == ""
+    redirect '/admin/edit/params[:id]', :error =>  "Merci de remplir l'ensemble des informations obligatoires" 
+  end
+  hommage = Hommage.get(params[:id])
+  nom = params[:nom]
+  dateDeces = params[:dateDeces] 
+  dateNaissance =  params[:dateNaissance] 
+  commentaires = params[:commentaire]
+
+  #pas de modif d'image
+  unless params[:image] && params[:image][:tempfile] && params[:image][:filename]
+      if hommage.update(    
+          :nom => nom, 
+          :dateNaissance => dateNaissance, 
+          :dateDeces => dateDeces, 
+          :commentaires => commentaires
+          )
+        redirect '/admin', :notice => "L'hommage a bien été modifié"
+      else
+        puts hommage.errors.inspect
+        redirect '/admin', :error => "Une erreur a empêché la modification de l'hommage - merci de contacter votre admin préféré"
+      end
+  end
+  #besoin de changer l'image - d'abord on supprime l'ancienne
+  client = picasa_client
+  client.delete(hommage.urlImageEdit)
+
+  #puis on crée la nouvelle
+  tmpfile = params[:image][:tempfile]
+  filename = params[:image][:filename]
+
+  file = Tempfile.new([filename, '.jpg'])
+  while blk = tmpfile.read(65536)
+    file.write(blk)
+  end
+  test_image = file.path
+  mime_type = 'image/jpeg'
+    
+  response = client.post_file('http://picasaweb.google.com/data/feed/api/user/default/albumid/default', test_image, mime_type).to_xml
+    
+  urlImagePleine = response.elements["media:group"].elements["media:content"].attributes['url']
+  urlImageTresReduite = response.elements["media:group"].elements["media:thumbnail[@width='144']"].attributes['url']
+  urlImageReduite = response.elements["media:group"].elements["media:thumbnail[@width='288']"].attributes['url']
+  urlImageEdit = response.elements["link[@rel='edit']"].attributes['href']
+  #client.delete(edit_uri)
+  tmpfile.close
+  tmpfile.unlink
+
+
+  if hommage.update(
+    :nom => nom, 
+    :dateNaissance => dateNaissance, 
+    :dateDeces => dateDeces, 
+    :commentaires => commentaires, 
+    :urlImagePleine => urlImagePleine, 
+    :urlImageReduite => urlImageReduite,
+    :urlImageTresReduite => urlImageTresReduite,
+    :urlImageEdit => urlImageEdit
+    )
+    redirect '/admin', :notice => "L'hommage a bien été modifié"
+  else
+    puts hommage.errors.inspect
+    redirect '/admin', :error => "Une erreur a empêché la modification de l'hommage - merci de contacter votre admin préféré"
+  end
+end
+
+
+get '/admin/delete/:id' do
+  protected!
+  @hommage = Hommage.get(params[:id])
+  unless @hommage
+   redirect '/admin', :error => "L'hommage n'existe pas - merci de contacter votre admin préféré"
+  end
+  urlImageEdit = @hommage.urlImageEdit
+  if @hommage.destroy
+    client = picasa_client
+    client.delete(urlImageEdit)
+    redirect '/admin', :notice => "L'hommage a été supprimé avec succès"
+  else
+    puts @hommage.errors.inspect
+    redirect '/admin', :error => "Une erreur a empêché la suppression de l'hommage - merci de contacter votre admin préféré"
+  end
+end
+   
+
 
 
 
